d97fc9afda1f04dbc11405d5fdb6ace2d1b802d2
Axel <axdiaz@users.noreply.github.com>
2020-03-27 20:08:38 -0300

    Add files via upload

---
+++ for.js
@@ -0,0 +1,13 @@
+//con cls en simbolo de sistema limpio la pantalla.
+
+//la estructura for nos sirve para repetir algo una cantidad de veces "que ya conocemos" .
+//declaracion:  var i = 0;  que es donde yo declaro la variable que voy a utilizar
+//condicion: i < 10 es donde yo pongo la condicion que va a regular la cantidad de repeticiones
+//incremental: i++ que es donde yo hago la variacion de la variable de control.
+
+for (var i = 0; i < 10; i++) {
+
+	console.log("estoy dentro del for");
+}
+
+console.log("se terminó el programa.")
---
+++ primerClaseWeb.js
@@ -0,0 +1,9 @@
+
+//esto es un comentario, significa que la computadora no ejecuta lo que esta despues de 2 barras.
+//var es una palabra clave que utilizamos para crear variables.
+var nombre = "Santiago"
+var apellido = "Rigoli"
+var edad = 30
+
+//esta funcion se utiliza para imprimir en consola.
+console.log("Mi nombre es " + nombre + " y mi apellido es " + apellido + " y mi edad es " + edad)
